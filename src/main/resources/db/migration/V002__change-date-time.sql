02ef502b3c135880dbca43f931ee30a37159633e
Adriano Braga Azevedo <adrianobragaza@gmail.com>
2021-05-22 18:54:40 -0300

    hotfix(db/migration) fix alter table

--- src/main/resources/db/migration/V002__change-date-time.sql
+++ src/main/resources/db/migration/V002__change-date-time.sql
@@ -1,14 +1,4 @@
-create schema if not exists currency_exchanges;
-    
-create table if not exists currency_exchanges.conversion_transaction
-(
-    id                       bigint          not null generated always as identity
-        constraint pk_conversion_transaction
-            primary key,
-    user_id                  bigint          not null,
-    origin_currency          varchar(5)      not null,
-    origin_value             numeric         not null,
-    destiny_currency         varchar(5)      not null,
-    conversion_rate          numeric         not null,
-    creation_date_time       timestamptz     not null
-);
+alter table currency_exchanges.conversion_transaction alter column id
+	set maxvalue 2147483647;
+
+alter table currency_exchanges.conversion_transaction alter column creation_date_time drop default;
